fd67670cd709224e4ea559ac09590cc59a6d9b14
Melissa Alves <alves.melissa1@aluno.ifsp.edu.br>
2025-08-12 17:48:41 -0300

    cria o crud do profile

--- src/server.js
+++ src/server.js
@@ -1,12 +1,54 @@
 import express from 'express'
 
 const app = express()
-const port = 3000
+const port = 3333
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+  app.post('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados)
+  res.json({
+    menssage: 'Usuário Criados com sucesso!',
+    profile: dados
+  })
+  })
+
+  app.put('/user', (req, res) => {
+   const dados = req.body 
+    console.log(dados)
+   res.json({
+    menssage: 'usuario editado com sucesso!',
+    profile: dados
+  })
+  })
+  
+  app.get('/profile', (req, res) => {
+  console.log('foi feito um GET no  /GET')
+  res.json({
+    profile: 'Dados listados com sucesso!'})
+  })
+ 
+  app.post('/profile', (req, res) => {
+  console.log('foi feito um POST no  /POST')
+  res.json({
+    profile: 'Dados Criados com sucesso!'})
+  })
+
+  app.put('/profile', (req, res) => {
+  console.log('foi feito um PUT no  /PUT')
+  res.json({
+    profile: 'Dados Atualizados com sucesso!'})
+  })
+
+  app.delete('/profile', (req, res) => {
+  console.log('foi feito um DELETE no  /DELETE')
+  res.json({
+    profile: 'Dados DELETADO com sucesso!'})
+  })
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`API Rodando em http://localhost:${port}`)
 })
