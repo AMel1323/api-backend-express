c0e43ebadf6da4bcb79e4375fc5e3260d33e63e1
Melissa Alves <alves.melissa1@aluno.ifsp.edu.br>
2025-11-03 15:53:26 -0300

    preparação da rota de login

---
+++ src/controllers/auth/loginController.js
@@ -0,0 +1,11 @@
+export const loginController = (req, res) => {
+    // receber o email e a senha 
+
+    // comparar se o email e a senha se batem com o que está no banco de dados
+
+    // se bater, gerar um token(JWT)
+
+    // enviar o token para o cliente
+
+    return
+}
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import authRouter from './routers/authRouter.js'
 import profileRouter from './routers/profileRouter.js'
 import productRouter from './routers/productRouter.js'
 import supplierRouter from './routers/supplierRouter.js'
@@ -15,11 +16,13 @@ app.use(logger)
 app.use(cors()) // liberar o acesso a API para qualquer aplicação (front-end)
 app.use(express.json()) // converter o JSON que chegou na requesição em um objeto JavaScript e vai salvar em req.body
 
+app.use('/auth', authRouter)
 app.use('/profile', profileRouter)
 app.use('/product', productRouter)
 app.use('/supplier', supplierRouter)
 app.use('/car', carRouter)
 app.use('/customer',  customerRouter)
+
 
 app.listen(port, () => {
   console.log(`API Rodando em http://localhost:${port}`)
